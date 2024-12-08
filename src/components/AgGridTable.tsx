7016eadf89778bb3a58f72219b611a2a3847e9f7
adhav712 <adhav712@gmail.com>
2024-12-08 23:46:05 +0530

    feat: Implement AgGridTable component for enhanced data display and export functionality in DriverTable

---
+++ src/components/AgGridTable.tsx
@@ -0,0 +1,82 @@
+import { useRef, useCallback } from 'react';
+import { Button, message } from 'antd';
+import { FileText, Sheet } from 'lucide-react';
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+import { ColDef, ColGroupDef } from 'ag-grid-community';
+import { useMediaQuery } from 'react-responsive';
+import { AgGridReact } from 'ag-grid-react';
+
+interface AgGridTableProps<T> {
+    columns: (ColDef | ColGroupDef)[];
+    data: T[];
+    isLoading: boolean;
+}
+
+const AgGridTable = <T,>({ columns, data, isLoading }: AgGridTableProps<T>) => {
+    const gridRef = useRef<AgGridReact>(null);
+
+    const onBtnExport = useCallback(() => {
+        if (!gridRef.current) {
+            message.error('No data to export');
+        }
+        gridRef.current!.api.exportDataAsCsv();
+    }, []);
+
+    // const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1224px)' })
+    // const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+    // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+    const isMobile = useMediaQuery({
+        query: '(max-width: 768px)'
+    });
+
+    return (
+        <div className="ag-theme-quartz mx-2" style={{ height: '60vh', width: 'auto' }}>
+            <div className="flex justify-end gap-2">
+                <Button
+                    icon={<FileText />}
+                    onClick={() => {
+                        message.info('Feature not available');
+                    }}
+                    className='mb-2'
+                />
+                <Button
+                    icon={<Sheet />}
+                    onClick={onBtnExport}
+                    className='mb-2'
+                />
+            </div>
+            <AgGridReact
+                ref={gridRef}
+                columnDefs={columns}
+                rowData={data}
+                loadingOverlayComponent={'Loading...'}
+                overlayNoRowsTemplate={'<span class="ag-overlay-loading-center">No rows to show</span>'}
+                pagination={isMobile ? false : true}
+                paginationPageSize={10}
+                paginationPageSizeSelector={[10, 25, 50, 100, 200, 500, 1000]}
+                domLayout='autoHeight'
+                loading={isLoading}
+                defaultColDef={{
+                    flex: 1,
+                    minWidth: 100,
+                    resizable: true,
+                    sortable: true,
+                    filter: true,
+                }}
+                enableCellTextSelection={true}
+            />
+            {isMobile && (
+                <div>
+                    <p className="text-xs text-gray-500 text-center mt-2">
+                        Swipe left to see more columns
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AgGridTable;
--- src/components/AlloyComponent/AlloyTable.tsx
+++ src/components/AlloyComponent/AlloyTable.tsx
@@ -1,8 +1,11 @@
 import React, { useMemo } from 'react';
-import { Table } from 'antd';
-import type { TableProps } from 'antd';
 import { FileImage } from 'lucide-react';
+import { CustomCellRendererProps } from 'ag-grid-react'; // React Data Grid Component
 import { AlloyMaster } from '@/pages/Alloy/Alloy.d';
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+import { ColDef, ColGroupDef } from 'ag-grid-community';
+import AgGridTable from '../AgGridTable';
 
 interface AlloyTableProps {
     data: AlloyMaster[];
@@ -10,69 +13,136 @@ interface AlloyTableProps {
 }
 
 const AlloyTable: React.FC<AlloyTableProps> = ({ data, isLoading }) => {
-    const columns: TableProps<AlloyMaster>['columns'] = useMemo(() => [
+    const columns: (ColDef | ColGroupDef)[] = useMemo(() => [
         {
-            title: 'S.N',
-            dataIndex: 'id',
-            key: 'id',
-            render: (_, __, index) => <span>{index + 1}</span>,
-            width: 'auto',
+            headerName: 'S.N',
+            field: 'id',
+            sortable: true,
+            filter: true,
+            width: 100,
+            valueGetter: 'node.rowIndex + 1'
         },
         {
-            title: 'Vehicle',
-            dataIndex: 'vehicle',
-            key: 'vehicle',
-            render: (_, data: Partial<AlloyMaster>) => <span
-                className="text-[#00008B] font-semibold cursor-pointer text-base "
-            >{data.vehicle}</span>,
-            sorter: (a, b) => a.vehicle.localeCompare(b.vehicle),
-            width: "auto",
+            headerName: 'Vehicle',
+            field: 'vehicle',
+            sortable: true,
+            filter: true,
+            cellRenderer: (params: CustomCellRendererProps) => (
+                <span className="text-[#00008B] font-semibold cursor-pointer text-base">
+                    {params.value}
+                </span>
+            )
         },
         {
-            title: 'Cus ID',
-            dataIndex: 'customerid',
-            key: 'customerid',
-            width: "auto",
-            render: (_, data: Partial<AlloyMaster>) => <span className="text-base">{data.customerid}</span>
+            headerName: 'Cus ID',
+            field: 'customerid',
+            sortable: true,
+            filter: true,
+            width: 100
         },
         {
-            title: 'Date',
-            dataIndex: 'date',
-            key: 'date',
-            width: "auto",
-            render: (_, data: Partial<AlloyMaster>) => <span className="text-base">{data.date}</span>
+            headerName: 'Date',
+            field: 'date',
+            sortable: true,
+            filter: true,
+            width: 150
         },
         {
-            title: 'Before PDF',
-            dataIndex: 'before',
-            key: 'before',
-            width: 'auto',
-            render: (_, data: Partial<AlloyMaster>) => (
+            headerName: 'Before PDF',
+            field: 'before',
+            sortable: false,
+            filter: false,
+            width: 150,
+            cellRenderer: (params: CustomCellRendererProps) => (
                 <a
-                    href={data?.before}
+                    href={params.value}
                     target="_blank"
                     rel="noreferrer"
-                    className="" >
-                    <FileImage />
+                    className="flex justify-center items-center"
+                >
+                    <FileImage className="cursor-pointer hover:text-blue-500" />
                 </a>
             )
         }
     ], []);
 
+    // const gridRef = useRef<AgGridReact>(null);
+
+    // const onBtnExport = useCallback(() => {
+    //     if (!gridRef.current) {
+    //         message.error('No data to export');
+    //     }
+    //     gridRef.current!.api.exportDataAsCsv();
+    // }, []);
+
+    // const isMobile = useMediaQuery({
+    //     query: '(max-width: 768px)'
+    // });
+
     return (
-        <Table<AlloyMaster>
+        // <>
+        //     <div className="ag-theme-quartz mx-2" style={{ height: '50vh', width: 'auto' }}>
+        //         <div className="flex justify-end gap-2">
+        //             <Button
+        //                 icon={<FileText />}
+        //                 onClick={() => {
+        //                     message.info('Feature not available');
+        //                 }}
+        //                 className='mb-2'
+        //             />
+        //             <Button
+        //                 icon={<Sheet />}
+        //                 onClick={onBtnExport}
+        //                 className='mb-2'
+        //             />
+        //         </div>
+        //         <AgGridReact
+        //             ref={gridRef}
+        //             columnDefs={columns}
+        //             rowData={data}
+        //             loading={isLoading}
+        //             loadingOverlayComponent={'Loading...'}
+        //             overlayNoRowsTemplate={'<span class="ag-overlay-loading-center">No rows to show</span>'}
+        //             pagination={isMobile ? false : true}
+        //             paginationPageSize={10}
+        //             paginationPageSizeSelector={
+        //                 isMobile ? false :
+        //                     [10, 25, 50, 100, 200, 500, 1000]}
+        //             domLayout='autoHeight'
+        //             defaultColDef={{
+        //                 flex: 1,
+        //                 minWidth: 100,
+        //                 resizable: true,
+        //                 sortable: true,
+        //                 filter: true,
+        //             }}
+        //             enableCellTextSelection={true}
+        //         />
+        //         {isMobile && (
+        //             <>
+        //                 {/* <div>
+        //                     <Pagination
+        //                         showSizeChanger
+        //                         showQuickJumper
+        //                         showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+        //                         pageSizeOptions={['10', '25', '50', '100', '200', '500', '1000']}
+        //                         className='mt-2'
+        //                         total={gridRef.current?.api.getDisplayedRowCount() || 0}
+        //                     />
+        //                 </div> */}
+        //                 <div>
+        //                     <p className="text-xs text-gray-500 text-center mt-2">
+        //                         Swipe left to see more columns
+        //                     </p>
+        //                 </div>
+        //             </>
+        //         )}
+        //     </div>
+        // </>
+        <AgGridTable
             columns={columns}
-            dataSource={data?.map((item, index) => ({ ...item, key: index })) ?? []}
-            loading={isLoading}
-            pagination={{
-                position: ['bottomRight'],
-                showSizeChanger: true,
-                pageSizeOptions: ['10', '20', '30', '40', '50'],
-                showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
-            }}
-            size="middle"
-            scroll={{ x: 'auto', y: '60vh' }}
-            className="p-2 border border-gray-200 rounded-md mx-2"
+            data={data}
+            isLoading={isLoading}
         />
     );
 };
